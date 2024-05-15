40414fb2b2389d2633067139c9e3cbf643285142
Viplav Nitin Khode <khodv01@pfw.edu>
2024-05-14 20:50:28 -0400

    Index Page Created - In-Progess

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,39 @@
+import { Button } from "@/components/ui/button";
+import { UserButton, auth } from "@clerk/nextjs";
+import { LogInIcon } from "lucide-react";
+import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+    const {userId} = await auth();
+    const isAuth = !!userId;
   return (
-    <main>
-      <h1 className="text-black-800 m-5"> Base of Project</h1>
-    </main>
+    <div className="w-screen min-h-screen bg-gradient-to-l from-gray-200 via-fuchsia-200 to-stone-100">
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ">
+            <div className="flex flex-col items-center text-center">
+                <div className="flex items-center">
+                    <h1 className="mr-3 text-5xl font-semibold"> Chat with any PDF</h1>
+                    <UserButton afterSignOutUrl="/" />
+                </div>
+                <div className="flex mt-2">
+                    {isAuth && <Button>Go to Chats</Button>}
+                </div>
+                <p className="max-w-l mt-2 text-lg text-slate-600">
+                    Empower your PDF experience with Hermes.  Upload your document, ask your questions, and 
+                    Hermes will efficiently guide you through your PDF journey
+                </p>
+                <div className="w-full mt-4">
+                    {isAuth ? (<h1>fileupload</h1>)
+                    : (
+                        <Link href="/sign-in">
+                            <Button>Login to get Started!
+                            <LogInIcon className="w-4 h-4 ml-2"/>
+                            </Button>
+                            
+                        </Link>
+                    )}
+                </div>
+            </div>
+        </div>
+    </div>
   );
 }
