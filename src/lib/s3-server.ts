476697f47045196b8e7d57357a5a5ae3e9765d6e
ViplavKhode <54805009+ViplavKhode@users.noreply.github.com>
2024-05-19 13:08:39 -0400

    Update s3-server.ts

--- src/lib/s3-server.ts
+++ src/lib/s3-server.ts
@@ -1,31 +1,37 @@
-import AWS from 'aws-sdk';
-import fs from 'fs';
+import { S3 } from "@aws-sdk/client-s3";
+import fs from "fs";
 
-export async function downloadFromS3(file_key:string) {
-    try{
-        AWS.config.update({
-            accessKeyId: process.env.NEXT_PUBLIC_S3_ACCESS_KEY_ID,
-            secretAccessKey: process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY_ID,
-        });
-        const s3 = new AWS.S3({
-            params:{
-                Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME,
-            },
-            region: 'us-east-2'
-        });
+export async function downloadFromS3(file_key: string): Promise<string> {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const s3 = new S3({
+        region: "us-east-2",
+        credentials: {
+          accessKeyId: process.env.NEXT_PUBLIC_S3_ACCESS_KEY_ID!,
+          secretAccessKey: process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY_ID!,
+        },
+      });
+      const params = {
+        Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME!,
+        Key: file_key,
+      };
 
-        const params={
-            Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME!,
-            Key: file_key,
-        };
-        
-        const obj = await s3.getObject(params).promise();
-        const file_name = `/temp/pdf-${Date.now()}.pdf`;
-        fs.writeFileSync(file_name, obj.Body as Buffer);
-        return file_name;
-    }catch(error){
-        console.log(error);
-        return null;
+      const obj = await s3.getObject(params);
+      const file_name = `./tmp/hermesproject${Date.now().toString()}.pdf`;
+
+      if (obj.Body instanceof require("stream").Readable) {
+        //open the writable stream and write the file
+        const file = fs.createWriteStream(file_name);
+        file.on("open", function (fd) {
+          obj.Body?.pipe(file).on("finish", () => {
+            return resolve(file_name);
+          });
+        });
+      }
+    } catch (error) {
+      console.error(error);
+      reject(error);
+      return null;
     }
-    
+  });
 }
